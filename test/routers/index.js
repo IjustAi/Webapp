5f47f51874d4ff8d3eacf047936332320d08eb0a
IjustAi <95865487+IjustAi@users.noreply.github.com>
2023-05-23 18:21:24 +0900

    Update index.js

--- test/routers/index.js
+++ test/routers/index.js
@@ -6,7 +6,7 @@ registerRouter = () => {
     let routers = [];
     glob.sync(resolve(__dirname, './', '**/*.js'))//at the same time
         .filter(value => (value.indexOf('index.js') === -1)) 
-        .map(router => {//router middleware
+        .map(router => {
             routers.push(require(router).routes())
             routers.push(require(router).allowedMethods())
         })
